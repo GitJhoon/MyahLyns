8dc8c76cb3ba755e74393ade42088aa89e0fa47e
GitJhoon <jonathan22office@gmail.com>
2026-06-27 20:11:08 -0300

    Update page status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,37 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js";
 
+async function status(request, response) {
+  const updateAt = new Date().toISOString();
 
-async function status(request, response){
+  const databaseVersionResult = await database.query("SHOW server_version;");
 
-	const result = await database.query("SELECT 1 + 1 as SUM;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
 
-	console.log(result);
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
 
-	return response.status(200).json({
-		chave: "Here is OK!"
-	});
+  const databaseMaxConnections =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = '" +
+      process.env.POSTGRES_DB +
+      "';",
+  );
+
+  const databaseOpenedConnections = databaseOpenedConnectionsResult.rows[0].count;
+
+  return response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: databaseMaxConnections,
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
---
+++ tests/integration/get.test.js
@@ -0,0 +1,5 @@
+test("GET index.js shoud return status code 200", async () => {
+  const response = await fetch("http://localhost:3000");
+
+  expect(response.status).toBe(200);
+});
